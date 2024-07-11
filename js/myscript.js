79cec5c045496b16f78eeb7e104b5f05f00ab00a
Alex Gordon <alexg1991@outlook.com>
2024-07-11 13:00:42 +0000

    Add two ways of styling elements

---
+++ js/myscript.js
@@ -0,0 +1,17 @@
+/* The (document).ready(function). The reason for this is that it's bad practice to run jQuery 
+before the page is fully loaded, as you might be trying to manipulate something that has not yet 
+been rendered on the webpage. So what we do here is put this ready function, and then anything 
+inside the curly brackets will only run once the DOM is fully loaded and once the HTML is fully rendered.*/
+
+/*
+$(document).ready(function() {
+    $("h2").css("text-decoration", "underline"); // underlines all h2 elements using inline style
+    $("ul").css("border", "solid 1px #ccc"); // adds a border to all lists using inline style
+});
+*/
+
+$(document).ready(function() {
+    $("h2").addClass("underline"); // underlines all h2 elements using class from style in html file
+    $("ul").addClass("border"); // adds a border to all lists using class from style in html file
+});
+
